33dad537219c148759adcc67f9c6899a3e73464d
Sudeep-A-Kulkarni <kulkarnisudeep2005@gmail.com>
2025-11-23 23:42:05 +0530

    Add files via upload

---
+++ Assignment32/Assignment32_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    int iCnt = 0;
+    if (iRow != iCol)
+    {
+        printf("please enter valid input ");
+        return;
+    }
+    
+    for (i = 1; i <= iRow; i++)
+    {   
+        for (j = 1; j <= iCol; j++)
+        {
+            if (i + j == (iRow+1))
+            {
+                printf("#\t");
+            }
+            else if( (i + j) < (iRow + 1))
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("@\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of colums : \n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
